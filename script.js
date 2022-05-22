add74e43a8cc3c71cf298b3a2650d05868cc86ee
MirThomasson <93678569+Mirthomasson@users.noreply.github.com>
2022-05-22 10:47:51 -0400

    desktop view

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-const rangeInputs = document.querySelectorAll('input[type="range"]')
-const numberInput = document.querySelector('input[type="number"]')
+const rangeInputs = document.querySelectorAll('input[type="range"]');
+const numberInput = document.querySelector('input[type="number"]');
 
 function handleInputChange(e) {
   let target = e.target
   if (e.target.type !== 'range') {
-    target = document.getElementById('range')
+    target = document.getElementById('range');
   } 
-  const min = target.min
-  const max = target.max
-  const val = target.value
-  
-  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
+  const min = target.min;
+  const max = target.max;
+  const val = target.value;
+
+  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%';
+  document.getElementById('gb-left').innerText = max - val;
 }
 
 rangeInputs.forEach(input => {
-  input.addEventListener('input', handleInputChange)
+  input.addEventListener('input', handleInputChange);
 })
 
-numberInput.addEventListener('input', handleInputChange)
+numberInput.addEventListener('input', handleInputChange);
+
